aca431a64f1bbe977c23011a04541a1f0289b4fc
Jon Dedman <jonathandedman@googlemail.com>
2024-10-13 23:10:26 +0100

    implement read of image and video

---
+++ read.py
@@ -0,0 +1,28 @@
+import cv2 as cv
+
+# Load an image using 'imread' specifying the path to image
+
+img = cv.imread('./Resources/Photos/cat_large.jpg')
+
+# Display the image using 'imshow'
+# cv.imshow('Cat', img)
+
+# reading videos
+
+# Capture a video using 'VideoCapture' specifying the path to video
+capture = cv.VideoCapture('./Resources/Videos/dog.mp4')
+
+# Loop through the video frame by frame
+while True:
+    (isTrue, frame) = capture.read()
+    cv.imshow('Video', frame)
+
+    # If the 'd' key is pressed, break from the loop
+    if cv.waitKey(20) & 0xFF == ord('b'):
+        break
+
+# Release the video capture object
+capture.release()
+
+# Wait for a key event and close the window
+# cv.waitKey(0)
